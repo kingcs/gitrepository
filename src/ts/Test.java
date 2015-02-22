31e7298d71ad2f14a12c235538ebb569f06404ea
kingcs <kingcs2008@126.com>
2015-02-22 22:25:25 +0800

    syso

--- src/ts/Test.java
+++ src/ts/Test.java
@@ -1,12 +1,12 @@
 package ts;
 
-import java.util.ArrayList;
-import java.util.List;
 
 public class Test {
 	
 	public static void main(String[] args) {
 		Object[] o = new Object[2147483647];
+		
+		System.out.println(o);
 
 	}
 
