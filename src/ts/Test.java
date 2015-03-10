1898bd9ec15a3028dca63f0fa440ddd1057e8836
kingcs <kingcs2008@126.com>
2015-03-10 09:26:05 +0800

    test map

--- src/ts/Test.java
+++ src/ts/Test.java
@@ -1,12 +1,27 @@
 package ts;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Test {
 
 	public static void main(String[] args) {
-		Object[] o = new Object[2147483647];
-
-		System.out.println(o);
-
+//		Object[] o = new Object[2147483647];
+//
+//		System.out.println(o);
+		
+		Map<String, Object> map = new HashMap<String, Object>();
+		map.put("id", 12345);
+		map.put("name", "zhangsan");
+		
+		for(String s : map.keySet()) {
+			System.out.println("key:" + s + " value:" + map.get(s));
+		}
+		
+		for(Map.Entry<String, Object> m : map.entrySet()) {
+			System.out.println(m.getKey() + "\t" + m.getValue());
+		}
+		
 	}
 
 }
