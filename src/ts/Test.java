514614e5bd945ec757f85c5d4b9dad923b286990
kingcs <kingcs2008@126.com>
2015-04-15 15:51:07 +0800

    commit

--- src/ts/Test.java
+++ src/ts/Test.java
@@ -1,26 +1,30 @@
 package ts;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class Test {
 
 	public static void main(String[] args) {
-//		Object[] o = new Object[2147483647];
-//
-//		System.out.println(o);
-		// test map
-		Map<String, Object> map = new HashMap<String, Object>();
-		map.put("id", 12345);
-		map.put("name", "zhangsan");
 		
-		for(String s : map.keySet()) {
-			System.out.println("key:" + s + " value:" + map.get(s));
-		}
+		String xml = "<s:Envelope xmlns:s=\"http://schemas.xmlsoap.org/soap/envelope/\">";
+		xml+= "  <s:Body>";
+		xml+= "    <SubmitSingleResponse xmlns=\"http://ThinkServer.Com/\">";
+		xml+= "      <SubmitSingleResult>";
+		xml+= "        <?xml version=\"1.0\" encoding=\"utf-8\"?>";
+		xml+= "        <CommandResponse batchIdentity=\"f89bcfdb-ea36-4132-938f-c88f214ae887\" seq=\"0\" xmlns=\"http://thinkserver.presalesadvisor.com/userservices/BatchResponseSchema.xsd\">";
+		xml+= "          <CommandResponseStatus>FailedUnkownRegion</CommandResponseStatus>";
+		xml+= "          <ResponseErrorDetail>UpdateUser command requires a valid RegionId if specified</ResponseErrorDetail>";
+		xml+= "        </CommandResponse>";
+		xml+= "      </SubmitSingleResult>";
+		xml+= "    </SubmitSingleResponse>";
+		xml+= "  </s:Body>";
+		xml+= "</s:Envelope>";
 		
-		for(Map.Entry<String, Object> m : map.entrySet()) {
-			System.out.println(m.getKey() + "\t" + m.getValue());
-		}
+		System.out.println(xml.substring(xml.lastIndexOf("<CommandResponseStatus"), xml.lastIndexOf("</CommandResponseStatus")));
+		System.out.println(xml.substring(xml.lastIndexOf("<ResponseErrorDetail"), xml.lastIndexOf("</ResponseErrorDetail")));
 	}
 
 }
